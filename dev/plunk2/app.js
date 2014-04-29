b2f62294573029c44e19047f1063991550e7feee
Kirk Peterkin <kirk@niiu.de>
2014-04-29 11:46:34 +0200

    Trying a clearer organized login manager
    Signed-off-by: Kirk Peterkin <kirk@niiu.de>

---
+++ dev/plunk2/app.js
@@ -0,0 +1,62 @@
+var app = angular.module('plunker', [])
+
+.config(function ($FBProvider) {
+  $FBProvider.setInitParams({
+    appId: '642106902524261'
+  });  
+})
+
+.controller('MainCtrl', function($scope, $FB, $window, $location) {
+  
+  updateLoginStatus(updateApiMe);
+
+  $scope.login = function () {
+    $FB.login(function (res) {
+      /**
+       * no manual $scope.$apply, I got that handled
+       */
+      if (res.authResponse) {
+        updateLoginStatus(updateApiMe);
+      }
+    }, {scope: 'email,user_likes'});
+  };
+
+  $scope.logout = function () {
+    $FB.logout(function () {
+      updateLoginStatus(updateApiMe);
+    });
+  };
+
+  $scope.share = function () {
+    $FB.ui(
+      {
+        method: 'feed',
+        from: "1368750610",
+        to: "7002535",
+        message: 'hey'
+      },
+      null
+    );
+  };
+
+  var autoToJSON = ['loginStatus', 'apiMe']; 
+  angular.forEach(autoToJSON, function (varName) {
+    $scope.$watch(varName, function (val) {
+      $scope[varName + 'JSON'] = JSON.stringify(val, null, 2);
+    }, true);
+  });
+  
+  function updateLoginStatus (more) {
+    $FB.getLoginStatus(function (res) {
+      $scope.loginStatus = res;
+
+      (more || angular.noop)();
+    });
+  }
+
+  function updateApiMe () {
+    $FB.api('/me', function (res) {
+      $scope.apiMe = res;
+    });
+  }
+});
