c6d114371d21fa0d06470b598fb7bc7a618c5e88
Kirk Peterkin <kirk@niiu.de>
2014-05-26 12:11:25 +0200

    getting users sections
    Signed-off-by: Kirk Peterkin <kirk@niiu.de>

--- app/scripts/controllers/userhome.js
+++ app/scripts/controllers/userhome.js
@@ -1,27 +1,87 @@
 'use strict';
 
 angular.module('niiuWebappApp')
-  .controller('UserhomeCtrl', ['$scope', 'niiuSyncer', 'localDB', function ($scope, niiuSyncer, localDB) {
+  .controller('UserhomeCtrl', ['$scope', 'niiuSyncer', 'localDB','$q', function ($scope, niiuSyncer, localDB, $q) {
 
   	console.log('do we have the scope.user?',$scope.user);
-  	localDB.getLastSync().then(
-  		function(sync_time) {
-  			    console.log('ive got the sync time in userHome' ,sync_time);
-
-  			    
-
-  			    console.log('now ive also got the update time', update_time);
 
 
-  			    var articleBlob = niiuSyncer.syncArticles($scope.user, sync_time, cp_update_time);
 
-  			    return articleBlob;
 
-  		},
-  		function(error) {
-  			console.log('we werent able to get a sync time, so we wont bother getting any articles', error);
+    var deferred = $q.defer();
 
-  		});
+
+    function getArticleList() {
+
+	  	
+	  	localDB.getLastSync().then(
+	  		function(sync_time) {
+	  			    console.log('ive got the sync time in userHome' ,sync_time);
+
+	  			    localDB.getLastProfileUpdate().then(function(update_time) {
+
+			  			    console.log('now ive also got the update time. So its time to send an article sync.', update_time);
+
+			  			    niiuSyncer.syncArticles($scope.user, sync_time, update_time).then(function(articleBlob) {
+
+			  			    	console.log('The api sync response looked like this',articleBlob);
+
+
+			  			    	deferred.resolve(articleBlob);
+			  			    	
+
+
+			  			    }, function(server_error) {
+			  			    		console.log('The api sync response looked like this',server_error);
+			  			    		deferred.reject(server_error);
+			  			    }
+
+
+
+			  			    	);
+			  			    
+
+			  			}
+				);
+
+	  		},
+	  		function(error) {
+	  			console.log('we werent able to get a sync time, so we wont bother getting any articles', error);
+	  			deferred.reject(error);
+
+	  		});
+
+	  	return deferred.promise;
+    } //end $scope.articleList
+
+//get the prior articles from the scope
+console.log('just seeing if theres a scope here',$scope);
+
+//replace the scope articles if we have new ones
+getArticleList().then( function(theList){
+	console.log('article list is finished',theList);
+	$scope.articles=theList.contents.data.articles;
+
+	//put the articles in the db
+	localDB.addArticlesToDB(theList.contents.data.articles);
+
+	},  function(noList) {
+		console.log('crap our attempt to get a list failed.',noList);
+	}
+	);
+
+
+/*
+ articleList.then(function(article_list) {
+ 		console.log('i am going to throw this article list up to the scope',article_list);
+ 		$scope.articles=article_list;
+ }
+
+
+ 	);
+
+*/
+
 
 
 /*
@@ -61,14 +121,26 @@ angular.module('niiuWebappApp')
 
 	*/
 
+niiuSyncer.sync3s().then(function(data_3s) {
+console.log('here is some 3s data',data_3s);
+	console.log('>>> here are the sections ' ,data_3s.data.contents.data.newSections);
+	//add sections to DB
+	localDB.addSectionsToDB(data_3s.data.contents.data.newSections);
+	//add sections to Scope
+	$scope.sections=data_3s.data.contents.data.newSections;
+	console.log('section 7 is called', $scope.sections[7].name);
+}, 
+function(no_data_3s) {
+	console.log('for some reason we couldnt get any 3s data',no_data_3s);
+}
+);
 
 
 
 
 
-
-
-	localDB.setLastSync();
+localDB.setLastSync();
+	
 
 
 
