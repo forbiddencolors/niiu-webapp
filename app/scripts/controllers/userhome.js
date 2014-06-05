1a09f794d98ae00ae3915160f980c838c87a897f
Kirk Peterkin <kirk@niiu.de>
2014-06-05 12:26:30 +0200

    getting articles on userhome from db
    not sure why this wasn't working exactly but I was getting an error about a duplicate index in the repeater. angular makes a pseudo id when it ng-repeats through an array, but you can override this and tell it which property of your array should be used as a key using hg-repeat track by (here in user home).
    
    Signed-off-by: Kirk Peterkin <kirk@niiu.de>

--- app/scripts/controllers/userhome.js
+++ app/scripts/controllers/userhome.js
@@ -59,21 +59,28 @@ console.log('just seeing if theres a scope here',$scope);
 
 console.log('are we online?',navigator.onLine);
 
-//replace the scope articles if we have new ones
-getArticleList().then( function(theList){
-	console.log('article list is finished',theList);
-	$scope.articles=theList.contents.data.articles;
 
-	//Put the articles into the service so we can get at them later
-	Articleservice.init(theList.contents.data.articles);
+function refreshArticles() {
 
-	//put the articles in the db so we can get to them if the user is offline or leaves the site
-	localDB.addArticlesToDB(theList.contents.data.articles);
+	//replace the scope articles if we have new ones
+	getArticleList().then( function(theList){
+			console.log('article list is finished',theList);
+			$scope.articles=theList.contents.data.articles;
 
-	},  function(noList) {
-		console.log('crap our attempt to get a list failed.',noList);
-	}
+			//Put the articles into the service so we can get at them later
+			Articleservice.init(theList.contents.data.articles);
+
+			//put the articles in the db so we can get to them if the user is offline or leaves the site
+			localDB.addArticlesToDB(theList.contents.data.articles);
+
+		},  function(noList) {
+			console.log('crap our attempt to get a list failed.',noList);
+		}
 	);
+
+}
+
+
 
 
 /*
@@ -126,32 +133,70 @@ getArticleList().then( function(theList){
 
 	*/
 
-niiuSyncer.sync3s().then(function(data_3s) {
-console.log('here is some 3s data',data_3s);
-	console.log('>>> here are the sections ' ,data_3s.data.contents.data.newSections);
-	//add sections to DB
-	localDB.addSectionsToDB(data_3s.data.contents.data.newSections);
-	localDB.addSourcesToDB(data_3s.data.contents.data.newSources);
-	localDB.addSourceSectionsToDB(data_3s.data.contents.data.newSourceSection);
-	localDB.addSectionSubsectionsToDB(data_3s.data.contents.data.newSectionSubsection);
-	localDB.addSubSectionsToDB(data_3s.data.contents.data.newSubsections);
-	localDB.addSourceSubsectionsToDB(data_3s.data.contents.data.newSourceSubsection);
-	//add sections to Scope
-	$scope.sections=data_3s.data.contents.data.newSections;
-	$scope.sources=data_3s.data.contents.data.newSources;
-	console.log('section 7 is called', $scope.sections[7].name);
-}, 
-function(no_data_3s) {
-	console.log('for some reason we couldnt get any 3s data',no_data_3s);
-}
-);
+	function refresh3s() {
+
+			niiuSyncer.sync3s().then(function(data_3s) {
+			console.log('here is some 3s data',data_3s);
+				console.log('>>> here are the sections ' ,data_3s.data.contents.data.newSections);
+				//add sections to DB
+				localDB.addSectionsToDB(data_3s.data.contents.data.newSections);
+				localDB.addSourcesToDB(data_3s.data.contents.data.newSources);
+				localDB.addSourceSectionsToDB(data_3s.data.contents.data.newSourceSection);
+				localDB.addSectionSubsectionsToDB(data_3s.data.contents.data.newSectionSubsection);
+				localDB.addSubSectionsToDB(data_3s.data.contents.data.newSubsections);
+				localDB.addSourceSubsectionsToDB(data_3s.data.contents.data.newSourceSubsection);
+				localDB.setLastSync();
+				//add sections to Scope
+				$scope.sections=data_3s.data.contents.data.newSections;
+				$scope.sources=data_3s.data.contents.data.newSources;
+				console.log('section 7 is called', $scope.sections[7].name);
+
+				//at this point we have all the 3s info and it should be saved so lets run
 
 
 
+			}, 
+			function(no_data_3s) {
+				console.log('for some reason we couldnt get any 3s data',no_data_3s);
+			}
+			);
+
+			
+	}
+
+	if (0) {
+		//currently we are refreshing everytime the page loads, but probably we should do this only 
+		//when last sync is a bit old maybe 10mins
+		//when people first login
+		//when people trigger a refresh (like dragging down in the ios app)
+		refresh3s();
+		refreshArticles();
+	} else {
+		//load3s();
+		console.log('lets get some articles!');
+		//put the article array into the service
+		localDB.loadArticlesFromDB().then( function(db_articles) {
+			console.log('got the following articles from the db',db_articles);
+			Articleservice.init(db_articles);
+
+			//works fine
+			//$scope.articles=[{title:'action jackson',content:'it goes like this'}];
+			$scope.articles=db_articles;
+
+			console.log($scope.articles[1]);
+			
 
 
-localDB.setLastSync();
-	
+		});
+
+
+
+		
+		
+
+	}
+
+
 
 
 
